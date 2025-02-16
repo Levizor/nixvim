f760a8ecc7e0dd73160dfaa6fdf3c9c064850068
Levizor <levizorri@gmail.com>
2025-02-16 15:01:30 +0100

    cmp

--- config/cmp.nix
+++ config/cmp.nix
@@ -34,89 +34,87 @@
             "kind"
             "menu"
           ];
-          /*
-              format =''
-              function(_, item)
-                local icons = {
-                  Namespace = "󰌗",
-                  Text = "󰉿",
-                  Method = "󰆧",
-                  Function = "󰆧",
-                  Constructor = "",
-                  Field = "󰜢",
-                  Variable = "󰀫",
-                  Class = "󰠱",
-                  Interface = "",
-                  Module = "",
-                  Property = "󰜢",
-                  Unit = "󰑭",
-                  Value = "󰎠",
-                  Enum = "",
-                  Keyword = "󰌋",
-                  Snippet = "",
-                  Color = "󰏘",
-                  File = "󰈚",
-                  Reference = "󰈇",
-                  Folder = "󰉋",
-                  EnumMember = "",
-                  Constant = "󰏿",
-                  Struct = "󰙅",
-                  Event = "",
-                  Operator = "󰆕",
-                  TypeParameter = "󰊄",
-                  Table = "",
-                  Object = "󰅩",
-                  Tag = "",
-                  Array = "[]",
-                  Boolean = "",
-                  Number = "",
-                  Null = "󰟢",
-                  String = "󰉿",
-                  Calendar = "",
-                  Watch = "󰥔",
-                  Package = "",
-                  Copilot = "",
-                  Codeium = "",
-                  TabNine = "",
-                }
+          format = ''
+            function(_, item)
+              local icons = {
+                Namespace = "󰌗",
+                Text = "󰉿",
+                Method = "󰆧",
+                Function = "󰆧",
+                Constructor = "",
+                Field = "󰜢",
+                Variable = "󰀫",
+                Class = "󰠱",
+                Interface = "",
+                Module = "",
+                Property = "󰜢",
+                Unit = "󰑭",
+                Value = "󰎠",
+                Enum = "",
+                Keyword = "󰌋",
+                Snippet = "",
+                Color = "󰏘",
+                File = "󰈚",
+                Reference = "󰈇",
+                Folder = "󰉋",
+                EnumMember = "",
+                Constant = "󰏿",
+                Struct = "󰙅",
+                Event = "",
+                Operator = "󰆕",
+                TypeParameter = "󰊄",
+                Table = "",
+                Object = "󰅩",
+                Tag = "",
+                Array = "[]",
+                Boolean = "",
+                Number = "",
+                Null = "󰟢",
+                String = "󰉿",
+                Calendar = "",
+                Watch = "󰥔",
+                Package = "",
+                Copilot = "",
+                Codeium = "",
+                TabNine = "",
+              }
 
-                local icon = icons[item.kind] or ""
-                item.kind = string.format("%s %s", icon, item.kind or "")
-                return item
-              end
-            '';
-          */
+              local icon = icons[item.kind] or ""
+              item.kind = string.format("%s %s", icon, item.kind or "")
+              return item
+            end
+          '';
         };
 
         window = {
           completion = {
-            winhighlight = "FloatBorder:CmpBorder,Normal:CmpPmenu,Search:PmenuSel";
+            winhighlight = "FloatBorder:CmpBorder,Normal:CmpPmenu,Search:PmenuSel,CursorLine:PmenuSel";
             scrollbar = false;
             sidePadding = 0;
-            border = [
-              "╭"
-              "─"
-              "╮"
-              "│"
-              "╯"
-              "─"
-              "╰"
-              "│"
-            ];
+            # border = [
+            #   "╭"
+            #   "─"
+            #   "╮"
+            #   "│"
+            #   "╯"
+            #   "─"
+            #   "╰"
+            #   "│"
+            # ];
           };
 
           settings.documentation = {
-            border = [
-              "╭"
-              "─"
-              "╮"
-              "│"
-              "╯"
-              "─"
-              "╰"
-              "│"
-            ];
-            winhighlight = "FloatBorder:CmpBorder,Normal:CmpPmenu,Search:PmenuSel";
+            # border = [
+            #   "╭"
+            #   "─"
+            #   "╮"
+            #   "│"
+            #   "╯"
+            #   "─"
+            #   "╰"
+            #   "│"
+            # ];
+            winhighlight = "FloatBorder:CmpBorder,Normal:CmpPmenu,Search:PmenuSel,CursorLine:PmenuSel";
           };
         };
       };
