a607b0fcec2f759b5e1de6db79fd52cfafb50b40
Levizor <levizorri@gmail.com>
2025-02-16 13:57:58 +0100

    textobj

--- config/lsp.nix
+++ config/lsp.nix
@@ -69,6 +69,14 @@
       lspInterop.enable = true;
       select = {
         enable = true;
+        lookahead = true;
+        keymaps = {
+          "af" = "@function.outer";
+          "if" = "@function.inner";
+          "ac" = "@class.outer";
+          "ic" = "@class.inner";
+          "as" = "@local.scope";
+        };
       };
     };
 
