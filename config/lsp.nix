370360ec9c304f656a327e02eb34eb04427f4ac7
Levizor <levizorri@gmail.com>
2025-02-24 00:05:19 +0100

    folding

--- config/lsp.nix
+++ config/lsp.nix
@@ -106,5 +106,19 @@
     trouble = {
       enable = true;
     };
+    nvim-ufo = {
+      enable = true;
+      setupLspCapabilities = true;
+
+      settings = {
+
+      };
+    };
+    config.settings = {
+      foldenable = true;
+      foldlevel = 99;
+      foldcolumn = "1";
+      foldlevelstart = 99;
+    };
   };
 }
