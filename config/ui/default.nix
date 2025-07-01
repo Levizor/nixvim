1ec8586d4ca6c4a0c80f1cc5248e527729d507f2
Levizor <levizorri@gmail.com>
2025-07-01 16:27:21 +0200

    upd

--- config/ui/default.nix
+++ config/ui/default.nix
@@ -14,9 +14,11 @@
     noice.enable = true;
     notify = {
       enable = true;
-      backgroundColour = "#000000";
-      level = "error";
-      render = "minimal";
+      settings = {
+        backgroundColour = "#000000";
+        level = "error";
+        render = "minimal";
+      };
     };
     render-markdown.enable = true;
     todo-comments.enable = true;
