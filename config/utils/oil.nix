75e11f52694964006fd53fd84a1cd65910884517
Levizor <levizorri@gmail.com>
2025-07-15 13:44:36 +0200

    upd view_options in oil

--- config/utils/oil.nix
+++ config/utils/oil.nix
@@ -6,6 +6,10 @@
         default_file_explorer = true;
         delete_to_trash = true;
         skip_confirmation_for_simple_edits = true;
+        view_options = {
+          show_hidden = true;
+          case_insensitive = false;
+        };
       };
     };
   };
