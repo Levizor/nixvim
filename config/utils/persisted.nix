45dc76bfdb6ff2bdb327a2ee97b4ece533066be2
Levizor <levizorri@gmail.com>
2025-08-16 13:50:56 +0200

    maybe this way?

--- config/utils/persisted.nix
+++ config/utils/persisted.nix
@@ -5,10 +5,12 @@
     enableTelescope = true;
     settings = {
       autoload.__raw = ''
-        if vim.fn.argc() > 0 and vim.fn.argv(0) == "-" then
-          return false
-        end
-        return true
+        function()
+          if vim.fn.argc() > 0 and vim.fn.argv(0) == "-" then
+            return false
+          end
+          return true
+        end,
       '';
       use_git_branch = true;
     };
