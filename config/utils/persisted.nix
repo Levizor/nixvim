5a8d060505d83841a31fdc3f468b6c08614ab446
Levizor <levizorri@gmail.com>
2025-08-16 13:19:26 +0200

    test

--- config/utils/persisted.nix
+++ config/utils/persisted.nix
@@ -7,18 +7,12 @@
       use_git_branch = true;
     };
 
-    luaConfig.post = ''
-      -- helper: detect if Neovim is reading from stdin
-      local function reading_from_stdin()
-        return (vim.fn.argc() == 1 and vim.fn.argv(0) == "-") or not vim.loop.isatty(0)
-      end
-
-      if not reading_from_stdin() then
-        require("persistence").setup({
-          -- your settings here
-        })
-      end
-
+    luaConfig.pre = ''
+      vim.api.nvim_create_autocmd("StdinReadPre", {
+        callback = function()
+          vim.g.persistence_enabled = false
+        end,
+      })
     '';
   };
 }
