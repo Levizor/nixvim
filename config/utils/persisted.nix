a450d341a1b6b2ed2335bd0a50a6251487054d83
Levizor <levizorri@gmail.com>
2025-08-16 14:05:56 +0200

    try this

--- config/utils/persisted.nix
+++ config/utils/persisted.nix
@@ -7,11 +7,15 @@
       use_git_branch = true;
       autoload.__raw = ''
         function()
-          if vim.fn.argc() > 0 and vim.fn.argv(0) == "-" then
+          -- vim.fn.getcmdwintype() is empty; stdin is available if `vim.fn.has("ttyin") == 0`
+          -- simpler: use `v:vim_did_enter` + check if stdin is attached
+          local stat = vim.loop.isatty(0)
+          if stat == false or vim.fn.stdin_readable() == 1 then
             return false
           end
           return true
-        end'';
+        end
+      '';
     };
   };
 }
