77d8701d4685b0271a6591435e98981a9115830d
Levizor <levizorri@gmail.com>
2025-08-16 13:43:17 +0200

    no way

--- config/utils/persisted.nix
+++ config/utils/persisted.nix
@@ -4,8 +4,14 @@
     enable = true;
     enableTelescope = true;
     settings = {
-      autoload = lib.nixvim.mkRaw ''
-        return false
+      autoload.__raw = ''
+        autoload = function()
+          -- If nvim was started with stdin ("-"), disable autoload
+          if vim.fn.argc() > 0 and vim.fn.argv(0) == "-" then
+            return false
+          end
+          return true
+        end,
       '';
       use_git_branch = true;
     };
