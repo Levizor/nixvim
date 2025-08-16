5098f86aa42695bd68d862d1ed78aea3983bb778
Levizor <levizorri@gmail.com>
2025-08-16 14:18:25 +0200

    prit

--- config/utils/persisted.nix
+++ config/utils/persisted.nix
@@ -7,11 +7,14 @@
       use_git_branch = true;
       autoload.__raw = ''
         function()
+          print("hello")
           -- returns false if stdin is readable
           local ok = vim.fn.filereadable("/dev/stdin")
           if ok == 1 then
+            print("false")
             return false
           end
+          print("true")
           return true
         end
       '';
