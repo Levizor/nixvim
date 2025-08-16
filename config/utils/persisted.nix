a09109b5e92bcfea5005db7a2915ff15a5830a05
Levizor <levizorri@gmail.com>
2025-08-16 13:13:35 +0200

    try

--- config/utils/persisted.nix
+++ config/utils/persisted.nix
@@ -3,9 +3,22 @@
     enable = false;
     enableTelescope = true;
     settings = {
-      autostart = true;
       autoload = true;
       use_git_branch = true;
     };
+
+    luaConfig.post = ''
+      -- helper: detect if Neovim is reading from stdin
+      local function reading_from_stdin()
+        return (vim.fn.argc() == 1 and vim.fn.argv(0) == "-") or not vim.loop.isatty(0)
+      end
+
+      if not reading_from_stdin() then
+        require("persistence").setup({
+          -- your settings here
+        })
+      end
+
+    '';
   };
 }
