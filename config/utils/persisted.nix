0e4696235bb895188749915de14a5d8c03d882e7
Levizor <levizorri@gmail.com>
2025-08-16 14:10:06 +0200

    uuuuugh

--- config/utils/persisted.nix
+++ config/utils/persisted.nix
@@ -5,16 +5,16 @@
     enableTelescope = true;
     settings = {
       use_git_branch = true;
-    };
-    autoload.__raw = ''
-      function()
-        -- returns false if stdin is readable
-        local ok = vim.fn.filereadable("/dev/stdin")
-        if ok == 1 then
-          return false
+      autoload.__raw = ''
+        function()
+          -- returns false if stdin is readable
+          local ok = vim.fn.filereadable("/dev/stdin")
+          if ok == 1 then
+            return false
+          end
+          return true
         end
-        return true
-      end
-    '';
+      '';
+    };
   };
 }
