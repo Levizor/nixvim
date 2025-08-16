2bbf655bcfd2328318827da1e2563265e22fb8ad
Levizor <levizorri@gmail.com>
2025-08-16 14:25:17 +0200

    false

--- config/utils/persisted.nix
+++ config/utils/persisted.nix
@@ -5,11 +5,12 @@
     enableTelescope = true;
     settings = {
       use_git_branch = true;
-      autoload.__raw = ''
-        function()
-          return false
-        end
-      '';
+      autoload = false;
+      # autoload.__raw = ''
+      #   function()
+      #     return false
+      #   end
+      # '';
     };
   };
 }
