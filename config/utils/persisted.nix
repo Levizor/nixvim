7fcfe4117efb76ac09be4d3530f6e0c1f90f82d8
Levizor <levizorri@gmail.com>
2025-08-16 13:28:10 +0200

    this works?

--- config/utils/persisted.nix
+++ config/utils/persisted.nix
@@ -3,7 +3,13 @@
     enable = true;
     enableTelescope = true;
     settings = {
-      autoload = false;
+      autoload = ''
+        -- If nvim was started with stdin ("-"), disable autoload
+        if vim.fn.argc() > 0 and vim.fn.argv(0) == "-" then
+          return false
+        end
+        return true
+      '';
       use_git_branch = true;
     };
 
