09c1e26566cd48379e77dc68be9b640e7469921a
Levizor <levizorri@gmail.com>
2025-08-16 14:09:27 +0200

    try this

--- config/utils/persisted.nix
+++ config/utils/persisted.nix
@@ -5,17 +5,16 @@
     enableTelescope = true;
     settings = {
       use_git_branch = true;
-      autoload.__raw = ''
-        function()
-          -- vim.fn.getcmdwintype() is empty; stdin is available if `vim.fn.has("ttyin") == 0`
-          -- simpler: use `v:vim_did_enter` + check if stdin is attached
-          local stat = vim.loop.isatty(0)
-          if stat == false or vim.fn.stdin_readable() == 1 then
-            return false
-          end
-          return true
-        end
-      '';
     };
+    autoload.__raw = ''
+      function()
+        -- returns false if stdin is readable
+        local ok = vim.fn.filereadable("/dev/stdin")
+        if ok == 1 then
+          return false
+        end
+        return true
+      end
+    '';
   };
 }
