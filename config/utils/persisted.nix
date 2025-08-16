016c9616bd9bd06cc06a1dfb2b4def14b5047386
Levizor <levizorri@gmail.com>
2025-08-16 13:45:30 +0200

    why???

--- config/utils/persisted.nix
+++ config/utils/persisted.nix
@@ -4,15 +4,16 @@
     enable = true;
     enableTelescope = true;
     settings = {
-      autoload.__raw = ''
-        autoload = function()
-          -- If nvim was started with stdin ("-"), disable autoload
-          if vim.fn.argc() > 0 and vim.fn.argv(0) == "-" then
-            return false
-          end
-          return true
-        end,
-      '';
+      autoload.__raw =
+        "
+        -- If nvim was started with stdin ("
+        - "), disable autoload
+        if vim.fn.argc() > 0 and vim.fn.argv(0) == "
+        - " then
+          return false
+        end
+        return true
+      ";
       use_git_branch = true;
     };
   };
