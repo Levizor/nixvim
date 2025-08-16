16552314bbe297db48222312df70e04afab76192
Levizor <levizorri@gmail.com>
2025-08-16 13:59:00 +0200

    maybe order

--- config/utils/persisted.nix
+++ config/utils/persisted.nix
@@ -4,6 +4,7 @@
     enable = true;
     enableTelescope = true;
     settings = {
+      use_git_branch = true;
       autoload = ''
         function()
           if vim.fn.argc() > 0 and vim.fn.argv(0) == "-" then
@@ -11,7 +12,6 @@
           end
           return true
         end'';
-      use_git_branch = true;
     };
   };
 }
