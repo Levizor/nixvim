4e5299838cd80a59c57956e22cba1459f5ce2d18
Levizor <levizorri@gmail.com>
2025-08-26 18:58:05 +0200

    no way

--- config/extraPlugins.nix
+++ config/extraPlugins.nix
@@ -2,9 +2,11 @@
 {
   extraPackages = with pkgs; [ jq ];
 
-  files = {
-    "lua/langswitch/init.lua" = { };
-  };
+  # files = {
+  #   "lua/langswitch/init.lua" = { };
+  # };
+
+  extraLuaPackages = [ ./lua ];
 
   extraConfigLua = ''
     require("langswitch").setup()
