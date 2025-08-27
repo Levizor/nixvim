659953e5807f2dffff6d9988dc7b5af4dab47307
Levizor <levizorri@gmail.com>
2025-08-27 16:18:01 +0200

    upd hash

--- config/extraPlugins.nix
+++ config/extraPlugins.nix
@@ -8,9 +8,9 @@
       version = "2025-08-26";
       src = pkgs.fetchFromGitHub {
         owner = "Levizor";
+        hash = "sha256-sWRJ4UK1H+lJjsiYikqekyqxy67hk4zC2G2Cct1u1Es=";
         repo = "hyprxkbswitch.nvim";
-        rev = "8dfe58e975110592fad0373c8d477412e6a85ccd";
-        sha256 = "sha256-93nNy8IAlqxjSWo1HFMy3B91UgTnGWXt9StX/vx0FYc";
+        rev = "67a00832864aad51b9343bd3c29358f67b316211";
       };
     })
   ];
